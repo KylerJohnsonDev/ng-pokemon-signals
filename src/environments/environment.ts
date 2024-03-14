25d673f6690fc08c3649622200fdb7fac454ec9a
Kyler Johnson <kylerjohnsondev@gmail.com>
2024-03-14 11:57:50 -0400

    add env files

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://ulalbbjosqiyqbixwidd.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsYWxiYmpvc3FpeXFiaXh3aWRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAyOTQ3NTksImV4cCI6MjAyNTg3MDc1OX0.RfSu-WkFXgbSrJzszlDpKnSbW59BkwfNsY4UIUkUYWk',
+};
