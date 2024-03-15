17bdaceb847a61293ca2495780d4fbb443776884
Ankita Sood <AnkitaSood@users.noreply.github.com>
2024-03-14 19:12:20 -0500

    convert app to pwa (#1)
    Co-authored-by: ankitasood <asood@octolabs.io>

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,9 @@ import { AppComponent } from './app/app.component';
 import { routes } from './routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
 import { authStore } from './app/auth.store';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -17,5 +18,9 @@ bootstrapApplication(AppComponent, {
     provideHttpClient(),
     provideAnimationsAsync(),
     importProvidersFrom([authStore]),
-  ],
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 }).catch((err) => console.error(err));
