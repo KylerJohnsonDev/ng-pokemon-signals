670669cc899801473515110540ffa556ff369b82
ankitasood <asood@octolabs.io>
2024-03-17 16:57:09 -0500

    install app prompt based on device OS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
-import { Component, HostBinding, inject } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { ButtonComponent } from './components/button.component';
-import { SignalInputDirective } from './directives/input.directive';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import {Component, HostBinding, inject} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {ButtonComponent} from './components/button.component';
+import {SignalInputDirective} from './directives/input.directive';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {
   ReactiveFormsModule,
 } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { PokemonStore } from './pokemon.store';
-import { TitleCasePipe } from '@angular/common';
-import { MatInputModule } from '@angular/material/input';
-import { authStore } from './auth.store';
-import { initFlowbite } from 'flowbite';
-import { NavbarComponent } from './components/navbar.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {PokemonStore} from './pokemon.store';
+import {TitleCasePipe} from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {authStore} from './auth.store';
+import {initFlowbite} from 'flowbite';
+import {NavbarComponent} from './components/navbar.component';
+import {InstallPromptComponent} from "./components/install-prompt.component";
+import {SmallScreenObserverStore} from "./small-screen-observer.store";
 
 @Component({
   selector: 'app-root',
@@ -27,19 +29,24 @@ import { NavbarComponent } from './components/navbar.component';
     TitleCasePipe,
     MatInputModule,
     NavbarComponent,
+    InstallPromptComponent,
   ],
   providers: [PokemonStore],
   template: `
-    <app-navbar />
+    <app-navbar/>
     <main class="flex w-full overflow-auto grow">
       <router-outlet></router-outlet>
     </main>
+    @defer (when smallScreenObserver.isSmallScreen()) {
+      <app-install-prompt/>
+    }
+
   `,
 })
 export class AppComponent {
   @HostBinding('class') class = 'flex flex-col h-full';
-
   readonly authStore = inject(authStore);
+  readonly smallScreenObserver = inject(SmallScreenObserverStore);
 
   constructor() {
     initFlowbite();
