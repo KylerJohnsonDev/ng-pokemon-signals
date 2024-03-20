a69b7c93dde025f98e7f699341f92dd79a038b02
ankitasood <asood@octolabs.io>
2024-03-19 22:37:22 -0600

    working prompt for both OS

--- src/app/components/install-prompt.component.ts
+++ src/app/components/install-prompt.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import {Platform} from "@angular/cdk/platform";
 
 @Component({
@@ -7,16 +7,16 @@ import {Platform} from "@angular/cdk/platform";
   imports: [],
   template: `
 
-    @if (modalPwaPlatform === 'IOS') {
-      <div id="toast-interactive" class="w-full p-4 text-gray-500 bg-white rounded-lg shadow" role="alert">
+    @if (isIos()) {
+      <div id="ios-toast-interactive" class="w-full p-4 text-gray-500 bg-white rounded-lg shadow" role="alert">
         <div class="flex">
           <div class="ms-3 text-sm font-normal">
             <span class="mb-1 text-sm font-semibold text-gray-900">Install Pokemon Companion on Home Screen</span>
             <div class="mb-2 text-sm font-normal">Tap the menu button and then select "Add to Home Screen".</div>
           </div>
-          <button type="button" (click)="closePwa()"
+          <button type="button"
                   class="ms-auto -mx-1.5 -my-1.5 bg-white items-center justify-center flex-shrink-0 text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8"
-                  data-dismiss-target="#toast-interactive" aria-label="Close">
+                  data-dismiss-target="#ios-toast-interactive" aria-label="Close">
             <span class="sr-only">Close</span>
             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                  viewBox="0 0 14 14">
@@ -27,8 +27,8 @@ import {Platform} from "@angular/cdk/platform";
         </div>
       </div>
     }
-    @if (modalPwaPlatform === 'ANDROID' && shouldInstall()) {
-      <div id="toast-interactive"
+    @if (isAndroid()) {
+      <div id="android-toast-interactive"
            class="w-full p-4 text-gray-500 bg-white rounded-lg shadow"
            role="alert">
         <div class="flex">
@@ -37,19 +37,19 @@ import {Platform} from "@angular/cdk/platform";
             <div class="mb-2 text-sm font-normal">Install Pokemon Companion App to your Home Screen.</div>
             <div class="grid grid-cols-2 gap-2">
               <div>
-                <a href="#" (click)="addToHomeScreen()"
-                   class="inline-flex justify-center w-full px-2 py-1.5 text-xs font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300">Install</a>
+                <button type="button" (click)="addToHomeScreen()"
+                   class="inline-flex justify-center w-full px-2 py-1.5 text-xs font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300">Install</button>
               </div>
               <div>
-                <a href="#" (click)="closePwa()"
+                <button type="button" data-dismiss-target="#android-toast-interactive"
                    class="inline-flex justify-center w-full px-2 py-1.5 text-xs font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200">Not
-                  now</a>
+                  now</button>
               </div>
             </div>
           </div>
-          <button type="button" (click)="closePwa()"
-                  class="ms-auto -mx-1.5 -my-1.5 bg-white items-center justify-center flex-shrink-0 text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-                  data-dismiss-target="#toast-interactive" aria-label="Close">
+          <button type="button"
+                  class="ms-auto -mx-1.5 -my-1.5 bg-white items-center justify-center flex-shrink-0 text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8"
+                  data-dismiss-target="#android-toast-interactive" aria-label="Close">
             <span class="sr-only">Close</span>
             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                  viewBox="0 0 14 14">
@@ -67,9 +67,9 @@ import {Platform} from "@angular/cdk/platform";
 })
 export class InstallPromptComponent implements OnInit {
   modalPwaEvent: any;
-  modalPwaPlatform: string | undefined;
   platform = inject(Platform);
-  installed = false;
+  isAndroid = signal(this.platform.ANDROID);
+  isIos = signal(this.platform.IOS);
 
   ngOnInit(): void {
     this.loadModalPwa();
@@ -81,7 +81,6 @@ export class InstallPromptComponent implements OnInit {
       window.addEventListener('beforeinstallprompt', (event: any) => {
         event.preventDefault();
         this.modalPwaEvent = event;
-        this.modalPwaPlatform = 'ANDROID';
       });
     }
 
@@ -89,7 +88,6 @@ export class InstallPromptComponent implements OnInit {
     if (this.platform.IOS && this.platform.SAFARI) {
       const isInStandaloneMode = ('standalone' in window.navigator) && ((<any>window.navigator)['standalone']);
       if (!isInStandaloneMode) {
-        this.modalPwaPlatform = 'IOS';
       }
     }
   }
@@ -102,9 +100,5 @@ export class InstallPromptComponent implements OnInit {
 
   private isRunningStandalone(): boolean {
     return (window.matchMedia('(display-mode: standalone)').matches);
-  }
-
-  closePwa(): void {
-    this.modalPwaPlatform = undefined;
   }
 }
